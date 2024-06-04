fffb8bfec8d5e740c7f72ac45585cff0125a2d3c
Shudarsan Regmi <shudarsanregmi555@gmail.com>
2024-06-04 08:11:10 +0530

    Python competitive programming note was  updated

--- aiml/cp_tricks/powerful_python_lines.py
+++ aiml/cp_tricks/powerful_python_lines.py
@@ -6,28 +6,28 @@
 
 ## Squaring the elements of a list
 
-```
+```python
 x = [1,2,3,4,5]
 x2 = [i**2 for i in x]
 ```
 
 ## Filtering list elements
 
-```
+```python
 x = [1,2,3,4,5]
 x2 = [i for i in x if x%2 == 0] # get the even elements
 ```
 ## Creating a list of tuples by merging two lists
 
-```
+```python
 x = [1,3,5,7]
 y = [2,4,6,8]
-a = [(i,j) for i in x for j in y]
+a = [(i,j) for i in x for j in y] # Creating all order pairs
 ```
 
 ## Creating a relationship between two sets(lists)
 
-```
+```python
 x = [1,3,5,7]
 y = ['a','b','c','d']
 rel = [(i,j) for i in x for j in y]
@@ -41,7 +41,7 @@ x = int(input().strip())
 
 ## Taking a list of integers as input
 ```
-x = list(map(int, input().strip.split())  
+x = list(map(int, input().strip().split())  
 ```
 
 
@@ -52,4 +52,28 @@ x = [[(1,2),(3,4)],[(5,6),(7,8)]]
 flattened = [num for sublist in x for tuples in sublist for num in tuples]
 ```
 
+
+
+### Increasing one of the indices of list based on condition
+
+```python
+a = [1,2,3]
+condition = True
+a[0 if condition else 1]
+```
+
+### Selecting one of the indices of a list out of many conditions
+
+```python
+a = [1,2,3]
+condition1 = False
+condition2 = False
+a[0 if condition1 else 1 if condition2 else 2]
+```
+
+### Initializing a list with multiple same elements
+
+```python
+arr = [0]*10
+```
 
