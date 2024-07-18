a6fc33eebbc88fc8adcdbd07da5c4ab52fb5ce1d
Shudarsan Regmi <65646203+ShudarsanRegmi@users.noreply.github.com>
2024-07-18 09:57:31 +0530

    Create file_handling.cpp

---
+++ Languages/file_handling.cpp
@@ -0,0 +1,16 @@
+# File Handling in C++
+
+- fstream library is used to work with files in c++
+
+Three classes in fastream library are used to create, write or read files.
+```
+  # classes
+  ofstream -> creates and writes to a file
+  ifstream -> reads from files
+  fstream -> A cmobination of ofstream and ifstrea: creates, reads, and writes to files 
+```
+
+### Creating and writing to a file using `ofstream`
+```cpp
+
+```
